2dee737590242886a5d153e1a4dc28a5bec206fd
Iambestfeed <95571916+Iambestfeed@users.noreply.github.com>
2024-05-01 09:46:31 +0700

    Update __init__.py

--- llm2vec/models/__init__.py
+++ llm2vec/models/__init__.py
@@ -1,2 +1,3 @@
 from .bidirectional_mistral import MistralBiModel, MistralBiForMNTP
 from .bidirectional_llama import LlamaBiModel, LlamaBiForMNTP
+from .bidirectional_qwen2 import Qwen2BiModel, Qwen2BiForMNTP
